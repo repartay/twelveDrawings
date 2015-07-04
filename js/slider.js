72da53f0149d04b796dbdc7dbd9b58b646c80bff
Margaret James <mpinaforejames@gmail.com>
2015-07-04 14:26:33 -0400

    carousel functionality, img accessibility, etc etc

---
+++ js/slider.js
@@ -0,0 +1,62 @@
+var cardWidth,
+	carouselPos;
+
+var numCards = 12,
+	carouselWrap = $('.carouselWrapper'),
+	carousel = $('.carousel'),
+	leftBtn = $('.left-btn'),
+	rightBtn = $('.right-btn');
+
+
+console.log(cardWidth);
+
+// Make function for event .resize on $(window) {
+	var cardWidth = $('.card').outerHeight();
+
+//}
+
+//apply attr('data-position', '1') to each carousel
+
+//event: leftBtn.click {
+	//this button parent .carousel {
+		//var oldPos = .carousel.attr('data-position');
+		//var currentPos = oldPos - 1;
+		//set data-attribute data-position = currentPos;
+		//fire function setPos(carousel);
+	//}
+//}
+
+//event: rightBtn.click {
+	//this button parent .carousel {
+		//var oldPos = .carousel.attr('data-position');
+		//var currentPos = oldPos + 1;
+		//set data-attribute data-position = currentPos;
+		//fire function setPos(carousel);
+	//}
+//}
+
+// var setPos = function(carousel) {
+
+//}
+
+//Click function
+
+$('body').on('click', function() {
+	var currentPos = $('.carousel').attr('data-position');
+	var currentPosNum = parseInt( currentPos, 10 ); 
+	var	newPos = currentPosNum + 1;
+
+		console.log(newPos);
+
+	$('.carousel').attr('data-position', newPos);
+});
+
+
+
+
+
+
+
+
+
+
